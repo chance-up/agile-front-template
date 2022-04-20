7661bfd7a3a36d3ac3ad3d3a2a648e543d4e86df
Hyelme <jhrim0810@naver.com>
2022-04-20 16:18:16 +0900

    Feat: 시스템 관리 List 컴포넌트 수정

--- src/components/layout/ListLayout.vue
+++ src/components/layout/ListLayout.vue
@@ -3,25 +3,41 @@
     <div class="tit-wrap">
       <h1 class="h1-tit">{{ title }}</h1>
     </div>
-    <SearchForm />
-    <ListForm :option="option" />
+    <slot name="search-form" />
+    <slot name="list-form" />
+    <!-- <SearchForm />
+    <ListForm :option="option">
+      <template slot="colGroup">
+        <colgroup>
+          <col width="10%" />
+          <col width="15%" />
+          <col width="*" />
+          <col width="10%" />
+          <col width="15%" />
+          <col width="10%" />
+        </colgroup>
+      </template>
+      <template slot="thead">
+        <thead>
+          <tr>
+            <th>No.</th>
+            <th>시스템명</th>
+            <th>시스템ID</th>
+            <th>담당자</th>
+            <th>Update</th>
+            <th>Action</th>
+          </tr>
+        </thead>
+      </template>
+    </ListForm> -->
   </article>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from 'vue-property-decorator';
 
-import SearchForm from '@/components/system/SearchForm.vue';
-import ListForm from '@/components/system/ListForm.vue';
-
-@Component({
-  components: {
-    SearchForm,
-    ListForm,
-  },
-})
+@Component
 export default class ListLayout extends Vue {
   @Prop() title!: string;
-  @Prop() option!: object;
 }
 </script>
--- src/pages/main/system/SystemListPage.vue
+++ src/pages/main/system/SystemListPage.vue
@@ -1,23 +1,74 @@
 <template>
-  <ListLayout :title="title" :option="listOption" />
+  <ListLayout :title="title">
+    <template slot="search-form">
+      <SearchForm />
+    </template>
+    <template slot="list-form">
+      <ListForm :title="listOption.listMainTitle">
+        <template slot="list-table">
+          <colgroup>
+            <col width="10%" />
+            <col width="15%" />
+            <col width="*" />
+            <col width="10%" />
+            <col width="15%" />
+            <col width="10%" />
+          </colgroup>
+          <thead>
+            <tr>
+              <th>No.</th>
+              <th>시스템명</th>
+              <th>시스템ID</th>
+              <th>담당자</th>
+              <th>Update</th>
+              <th>Action</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr v-for="(list, index) in listOption.listArrays" :key="index">
+              <td>{{ list.no }}</td>
+              <td>
+                <span class="bold">{{ list.systemName }}</span>
+              </td>
+              <td>{{ list.systemId }}</td>
+              <td>{{ list.manager }}</td>
+              <td>
+                <p>
+                  {{ list.update }}
+                </p>
+              </td>
+              <td>
+                <button class="mod_btn"><i>수정</i></button>
+                <button class="del_btn"><i>삭제</i></button>
+              </td>
+            </tr>
+          </tbody>
+        </template>
+      </ListForm>
+    </template>
+  </ListLayout>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 
 import ListLayout from '@/components/layout/ListLayout.vue';
+import SearchForm from '@/components/system/SearchForm.vue';
+import ListForm from '@/components/system/ListForm.vue';
 
 @Component({
   components: {
     ListLayout,
+    SearchForm,
+    ListForm,
   },
 })
 export default class SystemManagement extends Vue {
   title = '시스템 관리';
   listOption = {
     listMainTitle: '시스템 리스트',
-    listBtnTitle: '등록',
-    listItemTitle: ['No.', '시스템 ID', '시스템명', '담당자', 'Update'],
+    // listBtnTitle: '등록',
+    // listItemTitle: ['No.', '시스템 ID', '시스템명', '담당자', 'Update'],
     listArrays: [
       {
         no: '1',
