dd8f5689b676b6f4608790a7cf142df7cec4a748
Hyeongchang Kim <gudckd1013@gmail.com>
2022-04-21 21:35:22 +0900

    서비스 상세 api 테스트

--- src/store/modules/ServiceModule.ts
+++ src/store/modules/ServiceModule.ts
@@ -1,11 +1,24 @@
 import { GateWayResponse } from '@/types/GateWayResponse';
 import { ApiResponse } from '@/api/ApiResponse';
 import { Module, VuexModule, Mutation, Action } from 'vuex-module-decorators';
-import { ServiceResponse } from '@/types/ServiceType';
+import { ServiceResponse, DummyServiceResponse } from '@/types/ServiceType';
 import { addMock } from '@/api/AxiosClient';
 @Module({ name: 'ServiceModule' })
 export default class ServiceModule extends VuexModule {
   public services: ServiceResponse[] = [];
+  public service: DummyServiceResponse = {
+    serviceNm: '',
+    serviceId: '',
+    ManagerNm: '',
+    department: '',
+    email: '',
+    start_validity_date: '',
+    end_validity_date: '',
+    update_date: '',
+    authMethod: [],
+    slaPolicy: [],
+    serviceEx: '',
+  };
 
   @Mutation
   setServiceList(list: ServiceResponse[]): void {
@@ -23,5 +36,25 @@ export default class ServiceModule extends VuexModule {
 
     this.context.commit('setServiceList', response);
     console.log('SERVICE TEST', response);
+  }
+
+  @Mutation
+  setService(data: DummyServiceResponse): void {
+    this.service = data;
+  }
+
+  @Action
+  async getService(id: string) {
+    addMock(
+      '/service?serviceId=Service1',
+      '[{"serviceNm" : "Service Name1", "serviceId" : "Service1", "ManagerNm" : "홍길동", "department" : "Agile Core Team", "email" : "abcabc@kt.com", "start_validity_date": "2022-04-21", "end_validity_date": "2023-04-21", "update_date" : "2022-04-21", "authMethod" : ["Basic Auth","Auth_ID","Auth_PW"], "slaPolicy" : ["분", "30"], "serviceEx" : "서비스 설명입니다."}]'
+    );
+
+    const response = await ApiResponse.getInstance().get<GateWayResponse<DummyServiceResponse>>('/service', {
+      serviceId: id,
+    });
+    console.log('detail' + response);
+
+    this.context.commit('setService', response);
   }
 }
--- src/types/ServiceType.ts
+++ src/types/ServiceType.ts
@@ -8,28 +8,28 @@ export interface ServiceResponse {
 }
 
 export interface DummyServiceResponse {
-  no: string;
-  serviceName: string;
+  serviceNm: string;
   serviceId: string;
-  ManagerName: string;
+  ManagerNm: string;
   department: string;
   email: string;
-  period: string;
-  update: string;
+  start_validity_date: string;
+  end_validity_date: string;
+  update_date: string;
   authMethod: string[];
   slaPolicy: string[];
   serviceEx: string;
 }
 
 export const dummyData: DummyServiceResponse = {
-  no: '1',
-  serviceName: '서비스A',
+  serviceNm: '서비스A',
   serviceId: 'SERVICE ID_NUMBER',
-  ManagerName: '홍길동',
+  ManagerNm: '홍길동',
   department: 'Agile Core Team',
   email: 'abcabc@kt.com',
-  period: '2202-04-01 ~ 2022-04-01',
-  update: 'YYYY-MM-DD hh:mm:ss',
+  start_validity_date: '2022-04-01',
+  end_validity_date: '2022-04-01',
+  update_date: 'YYYY-MM-DD hh:mm:ss',
   authMethod: ['Basic Auth', 'passp2p_WOOOO', 'passp2p_WOOOO'],
   slaPolicy: ['분', '30'],
   serviceEx: '서비스 설명',
