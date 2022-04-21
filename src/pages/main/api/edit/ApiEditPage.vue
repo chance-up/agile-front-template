d0b55b9d0cce112fcd2bfaa85f8b2890d8dd57b3
feelincoding <feelincoding@gmail.com>
2022-04-21 16:22:40 +0900

    router/ edit 경로 추가

---
+++ src/pages/main/api/edit/ApiEditPage.vue
@@ -0,0 +1,49 @@
+<template>
+  <ContentLayout title="시스템 정보 수정" subTitle="기본정보 수정" depth="시스템 관리">
+    <template v-slot:contents>
+      <ul>
+        <InputGroup inputNm="시스템명" :place="dumyData.sysNm" inputClass="input-box lg" :disabled="true" />
+        <InputGroup inputNm="시스템 ID" :place="dumyData.sysId" inputClass="input-box lg" :disabled="true" />
+        <InputGroup
+          inputNm="담당자 이름"
+          :place="dumyData.mgrNm"
+          inputClass="input-box lg check-false"
+          validCheck="중복된 API ID 입니다."
+        />
+        <InputGroup inputNm="소속" :place="dumyData.belong" inputClass="input-box lg check-ok" />
+        <InputGroup inputNm="E-mail" :place="dumyData.eMail" inputClass="input-box lg check-ok" />
+        <LinkGroup inputNm="연동방식" :linkType="dumyData.linkType" />
+        <TextAreaGroup inputNm="시스템 설명" />
+      </ul>
+    </template>
+    <template v-slot:buttons>
+      <div class="btn-wrap">
+        <button class="lg-btn purple-btn">저장</button>
+        <button class="lg-btn white-btn">취소</button>
+      </div>
+    </template>
+  </ContentLayout>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+import ContentLayout from '@/components/layout/ContentLayout.vue';
+import InputGroup from '@/components/system/InputGroup.vue';
+import LinkGroup from '@/components/system/LinkGroup.vue';
+import TextAreaGroup from '@/components/system/TextAreaGroup.vue';
+import { DummySystemResponse, dummyData } from '@/types/SystemType';
+
+@Component({
+  components: {
+    ContentLayout,
+    InputGroup,
+    LinkGroup,
+    TextAreaGroup,
+  },
+})
+export default class ApiEditPage extends Vue {
+  get dumyData(): DummySystemResponse {
+    return dummyData;
+  }
+}
+</script>
+<style lang=""></style>
