891a6a0da40042765ced3da5a414f332813d7767
Hyelme <jhrim0810@naver.com>
2022-04-22 13:13:15 +0900

    Feat: 시스템 모듈 수정

--- src/store/modules/SystemModule.ts
+++ src/store/modules/SystemModule.ts
@@ -1,5 +1,5 @@
 import { GateWayResponse } from '@/types/GateWayResponse';
-import { SystemResponse } from '@/types/SystemType';
+import { SystemResponse, SystemRes } from '@/types/SystemType';
 import { SearchCondition } from '@/types/SearchType';
 import { ApiResponse } from '@/api/ApiResponse';
 import { addMock } from '@/api/AxiosClient';
@@ -39,9 +39,9 @@ export default class SystemModule extends VuexModule {
     //   '/system/list/',
     //   '[{"id": 1,"nm": "systemName1", "tkcgrNm": "managerName1","tkcgrPos": "managerPosition1","tkcgrEml": "managerEmail1","ifGrp": "interfaceGroup1","desc": "systemDescription1","createdAt": "2020-01-01","createdBy": "systemUser1","updatedAt": null,"updatedBy": null},{"id": 1,"nm": "systemName1", "tkcgrNm": "managerName1","tkcgrPos": "managerPosition1","tkcgrEml": "managerEmail1","ifGrp": "interfaceGroup1","desc": "systemDescription1","createdAt": "2020-01-01","createdBy": "systemUser1","updatedAt": null,"updatedBy": null},{"id": 1,"nm": "systemName1", "tkcgrNm": "managerName1","tkcgrPos": "managerPosition1","tkcgrEml": "managerEmail1","ifGrp": "interfaceGroup1","desc": "systemDescription1","createdAt": "2020-01-01","createdBy": "systemUser1","updatedAt": null,"updatedBy": null},{"id": 1,"nm": "systemName1", "tkcgrNm": "managerName1","tkcgrPos": "managerPosition1","tkcgrEml": "managerEmail1","ifGrp": "interfaceGroup1","desc": "systemDescription1","createdAt": "2020-01-01","createdBy": "systemUser1","updatedAt": null,"updatedBy": null},{"id": 1,"nm": "systemName1", "tkcgrNm": "managerName1","tkcgrPos": "managerPosition1","tkcgrEml": "managerEmail1","ifGrp": "interfaceGroup1","desc": "systemDescription1","createdAt": "2020-01-01","createdBy": "systemUser1","updatedAt": null,"updatedBy": null},{"id": 1,"nm": "systemName1", "tkcgrNm": "managerName1","tkcgrPos": "managerPosition1","tkcgrEml": "managerEmail1","ifGrp": "interfaceGroup1","desc": "systemDescription1","createdAt": "2020-01-01","createdBy": "systemUser1","updatedAt": null,"updatedBy": null}]'
     // );
-    const response = await ApiResponse.getInstance().get<GateWayResponse<SystemResponse[]>>('/system/list');
-    console.log('listOption', typeof response);
-    this.context.commit('setSystemList', response.data.values);
+    const response = await ApiResponse.getInstance().get<GateWayResponse<SystemRes>>('/system/list');
+    console.log('listOption', response.data.value);
+    this.context.commit('setSystemList', response.data.value);
 
     // this.context.commit('setSystemList', response.data);
   }
@@ -50,7 +50,7 @@ export default class SystemModule extends VuexModule {
   async getSystemDetail(id: number) {
     // addMock(`/system/detail/${id}`, JSON.stringify(dummyData));
     const response = await ApiResponse.getInstance().get<GateWayResponse<SystemResponse>>(`/system/detail/${id}`);
-    console.log('response', response);
-    this.context.commit('setSystemItem', response);
+    console.log('response', response.data);
+    return response.data;
   }
 }
