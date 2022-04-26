861c96c41a3d7a9907d4ea7adfbc446aa1f10b6c
feelincoding <feelincoding@gmail.com>
2022-04-26 09:00:17 +0900

    modal 구현 시작

---
+++ src/components/commons/modal/Modal.vue
@@ -0,0 +1,9 @@
+<template>
+  <div></div>
+</template>
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+export default class Modal extends Vue {}
+</script>
+<style lang=""></style>
