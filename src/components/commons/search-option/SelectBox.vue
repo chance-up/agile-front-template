4012159c84dc4325480dab1a49758722d52321c6
sloppy-cat <js3891129@gmail.com>
2022-04-29 09:56:09 +0900

    :fix API관리 리스트 검색 선택박스 변경

--- src/components/commons/search-option/SelectBox.vue
+++ src/components/commons/search-option/SelectBox.vue
@@ -1,32 +1,39 @@
 <template>
   <div>
     <label class="label">{{ label }}</label>
-    <select class="select-box" @change="update">
-      <option v-for="(item, index) in selectOptions" :key="index" :value="item.value">
-        {{ item.label }}
+    <select class="select-box" @change="handleChangeTarget">
+      <option v-for="(item, index) in selectOptions" :key="index">
+        {{ item }}
       </option>
     </select>
-    <input type="text" class="input-box" :value="value" @input="inputTxt" :placeholder="placeholder" />
+    <input type="text" class="input-box" :value="searchData.value" :placeholder="placeholder" />
   </div>
 </template>
 
 <script lang="ts">
-import { Component, Prop, Vue } from 'vue-property-decorator';
+import { SelectOptionType } from '@/types/SearchType';
+import { Component, Prop, Vue, Watch } from 'vue-property-decorator';
 
 @Component
 export default class SelectBox extends Vue {
   @Prop() public label!: string;
   @Prop() public placeholder!: string;
   @Prop() public selectOptions!: string[];
-  @Prop() public value!: string;
+  @Prop() public value!: SelectOptionType;
 
-  update($event: any) {
-    console.log('selected: ', $event.target.value);
-    this.$emit('update:value', $event.target.value);
+  searchData: SelectOptionType = {
+    label: '',
+    value: '',
+  };
+
+  handleChangeTarget(event: any) {
+    this.searchData.label = event.target.value;
   }
 
-  inputTxt($event: any) {
-    this.$emit('input', $event.target.value);
+  @Watch('value')
+  handleChangeValue(value: SelectOptionType) {
+    this.searchData.value = value.value;
+    this.searchData.label = value.label;
   }
 }
 </script>
