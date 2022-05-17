7bca4140caa6c0bc43b3300131c8c253907d4277
Hyelme <jhrim0810@naver.com>
2022-05-17 15:19:31 +0900

    Feat: API Top 5 progress bar 추가

---
+++ src/components/commons/ProgressBar.vue
@@ -0,0 +1,14 @@
+<template>
+  <b-progress class="mt-2" :max="100" show-value>
+    <b-progress-bar :value="70" :max="100" variant="success"></b-progress-bar>
+  </b-progress>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+
+@Component
+export default class ApiTop5 extends Vue {
+  @Prop() public item: any;
+}
+</script>
