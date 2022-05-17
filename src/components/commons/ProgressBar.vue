8d35ae7b2d3073f976111cb29c3c989792ec4a4b
Hyelme <jhrim0810@naver.com>
2022-05-17 17:18:39 +0900

    Feat: top 5 progress bar 수정

--- src/components/commons/ProgressBar.vue
+++ src/components/commons/ProgressBar.vue
@@ -1,7 +1,12 @@
 <template>
-  <b-progress class="mt-2" :max="100" show-value>
-    <b-progress-bar :value="70" :max="100" variant="success"></b-progress-bar>
+  <b-progress class="w-25" style="border-radius: 10px">
+    <b-progress-bar
+      :value="listItem.success"
+      :max="listItem.total"
+      style="border-radius: 10px; background-color: #6998ff"
+    ></b-progress-bar>
   </b-progress>
+  <!-- <div id="apiTop5List" data-echart-responsive="true"></div> -->
 </template>
 
 <script lang="ts">
@@ -9,6 +14,11 @@ import { Component, Prop, Vue } from 'vue-property-decorator';
 
 @Component
 export default class ApiTop5 extends Vue {
-  @Prop() public item: any;
+  @Prop() public listItem!: any;
 }
 </script>
+<style>
+.progress {
+  transition: width 3.6s ease;
+}
+</style>
