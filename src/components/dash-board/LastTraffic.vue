66e04d1f3522b9e1650f0082541ffdbcd4342626
sloppy-cat <js3891129@gmail.com>
2022-05-18 09:31:05 +0900

    feat: 대시보드 추이 데이터변경

--- src/components/dash-board/LastTraffic.vue
+++ src/components/dash-board/LastTraffic.vue
@@ -11,136 +11,113 @@ import { drawChart } from '@/utils/chart';
 @Component
 export default class LastTraffic extends Vue {
   mounted() {
-    drawChart('lastTraffic', this.lastTrafficOption);
+    for (let h = 0; h < 24; h++) {
+      for (let m = 0; m < 6; m++) {
+        this.trafcStatTrnd.push({
+          statBaseTm: `2019-08-01 ${h < 10 ? '0' + h : h}:${m + '0'}:00`,
+          todayCnt: this.randomInt(0, 1000),
+          lastDayCnt: this.randomInt(0, 1000),
+        });
+      }
+    }
+    const lastTrafficOption: echarts.EChartsOption = {
+      legend: {
+        data: ['today, lastDay'],
+      },
+      tooltip: {
+        triggerOn: 'none',
+        position: function (pt: any) {
+          return [pt[0], 130];
+        },
+      },
+      toolbox: {
+        left: 'right',
+        itemSize: 20,
+        top: -5,
+        feature: {
+          dataZoom: {
+            yAxisIndex: 'none',
+          },
+          restore: {},
+        },
+      },
+      xAxis: {
+        type: 'category',
+        boundaryGap: false,
+        data: this.trafcStatTrnd.map((item: any) => item.statBaseTm.slice(11, 16)),
+
+        axisPointer: {
+          type: 'line',
+          label: {
+            backgroundColor: '#777',
+          },
+        },
+        axisLabel: {
+          inside: true,
+          formatter: '{value}\n',
+        },
+      },
+      axisPointer: {
+        label: {
+          backgroundColor: '#777',
+        },
+      },
+
+      yAxis: {
+        type: 'value',
+        axisTick: {
+          inside: true,
+        },
+        splitLine: {
+          show: true,
+        },
+        axisLabel: {
+          inside: true,
+          formatter: '{value}\n',
+        },
+        z: 10,
+      },
+      grid: {
+        top: 0,
+        left: 0,
+        right: 0,
+        height: 148,
+      },
+      dataZoom: [
+        {
+          type: 'inside',
+          throttle: 50,
+        },
+      ],
+      series: [
+        {
+          name: 'today',
+          type: 'line',
+          symbol: 'circle',
+          symbolSize: 5,
+          data: this.trafcStatTrnd.map((item: any) => item.todayCnt),
+        },
+        {
+          name: 'lastDay',
+          type: 'line',
+          symbol: 'circle',
+          symbolSize: 5,
+          data: this.trafcStatTrnd.map((item: any) => item.lastDayCnt),
+        },
+      ],
+    };
+    drawChart('lastTraffic', lastTrafficOption);
   }
   // base = new Date();
   // today = new Date(this.base.getTime()).toISOString().slice(0, 10);
   // lastDay = new Date(this.base.getTime() - 86400000).toISOString().slice(0, 10);
   // lastWeek = new Date(this.base.getTime() - 604800000).toISOString().slice(0, 10);
-  data1 = [100, 200, 300];
-  data2 = [400, 500, 600];
-  lastTrafficOption: echarts.EChartsOption = {
-    legend: {
-      data: ['today, lastDay'],
-    },
-    tooltip: {
-      triggerOn: 'none',
-      position: function (pt: any) {
-        return [pt[0], 130];
-      },
-    },
-    toolbox: {
-      left: 'right',
-      itemSize: 20,
-      top: -5,
-      feature: {
-        dataZoom: {
-          yAxisIndex: 'none',
-        },
-        restore: {},
-      },
-    },
-    xAxis: {
-      type: 'time',
-      axisPointer: {
-        value: new Date().getUTCDay(),
-        snap: true,
-        lineStyle: {
-          color: '#7581BD',
-          width: 2,
-        },
-        label: {
-          show: true,
-          backgroundColor: '#7581BD',
-        },
-        handle: {
-          show: true,
-          color: '#7581BD',
-        },
-      },
-      splitLine: {
-        show: true,
-      },
-    },
-    yAxis: {
-      type: 'value',
-      axisTick: {
-        inside: true,
-      },
-      splitLine: {
-        show: false,
-      },
-      axisLabel: {
-        inside: true,
-        formatter: '{value}\n',
-      },
-      z: 10,
-    },
-    grid: {
-      top: 0,
-      left: 0,
-      right: 0,
-      height: 148,
-    },
-    dataZoom: [
-      {
-        type: 'inside',
-        throttle: 50,
-      },
-    ],
-    series: [
-      {
-        name: 'today',
-        type: 'line',
-        smooth: true,
-        symbol: 'circle',
-        symbolSize: 5,
-        sampling: 'average',
-        itemStyle: {
-          color: '#0770FF',
-        },
-        stack: 'a',
-        areaStyle: {
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            {
-              offset: 0,
-              color: 'rgba(58,77,233,0.8)',
-            },
-            {
-              offset: 1,
-              color: 'rgba(58,77,233,0.3)',
-            },
-          ]),
-        },
-        data: this.data1,
-      },
-      {
-        name: 'lastDay',
-        type: 'line',
-        smooth: true,
-        stack: 'a',
-        symbol: 'circle',
-        symbolSize: 5,
-        sampling: 'average',
-        itemStyle: {
-          color: '#F2597F',
-        },
-        areaStyle: {
-          color: new echarts.graphic.LinearGradient(0, 0, 0, 1, [
-            {
-              offset: 0,
-              color: 'rgba(213,72,120,0.8)',
-            },
-            {
-              offset: 1,
-              color: 'rgba(213,72,120,0.3)',
-            },
-          ]),
-        },
-        data: this.data2,
-      },
-    ],
+
+  randomInt = (min: number, max: number) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
   };
+
+  trafcStatTrnd: { statBaseTm: string; todayCnt: number; lastDayCnt: number }[] = [];
 }
 </script>
 <style></style>
