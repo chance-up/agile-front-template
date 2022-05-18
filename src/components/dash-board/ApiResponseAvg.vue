52dd6846d006d81dfd691979dd32bc355257684d
HyeongChangKim <gudckd1013@gmail.com>
2022-05-18 18:32:41 +0900

    feat : 대시보드 평균응답 모달

--- src/components/dash-board/ApiResponseAvg.vue
+++ src/components/dash-board/ApiResponseAvg.vue
@@ -1,7 +1,14 @@
 <template>
   <div class="chart-wrap">
     <h3 class="h3-tit">API 평균 응답시간 및 TPS</h3>
-    <div class="chart-group tps-group">
+    <div
+      class="chart-group tps-group"
+      :class="{
+        'boxWidth mouse-hover': modal == false,
+        'expand-modal': modal == true,
+      }"
+      @click="showModalDetail()"
+    >
       <div class="tps-chart">
         <i><img src="@/assets/req_ico.svg" alt="평균 응답시간" /></i>
         <p class="text">평균 응답시간<em>(종전 1분)</em></p>
@@ -18,6 +25,48 @@
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
 @Component
-export default class ApiResponseAvg extends Vue {}
+export default class ApiResponseAvg extends Vue {
+  modal = false;
+  showModalDetail() {
+    this.setBoxWidth();
+
+    setTimeout(() => {
+      this.modal = true;
+    }, 0);
+  }
+  hideModalDetail() {
+    this.modal = false;
+  }
+  widthValue = '';
+
+  setBoxWidth() {
+    const val = document.querySelector('.chart-group')?.clientWidth;
+    this.widthValue = `${val}px`;
+    document.documentElement.style.setProperty('--box-width', this.widthValue);
+  }
+}
 </script>
-<style></style>
+<style scoped>
+:root {
+  --box-width: 100%;
+}
+.mouse-hover:hover {
+  box-shadow: 0 0 11px rgba(33, 33, 33, 0.3);
+}
+
+.boxWidth {
+  width: var(--box-width);
+}
+
+.expand-modal {
+  width: 50%;
+  z-index: 5;
+  position: absolute;
+  transform: translate(-87.6%, 50%) scaleY(2);
+  align-items: center;
+  flex-direction: column;
+  justify-content: space-between;
+  box-shadow: 0 0 11px rgba(33, 33, 33, 0.3);
+  transition: all 0.5s;
+}
+</style>
