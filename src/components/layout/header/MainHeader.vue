5b9074592e7066060430239ac9d18c452e763377
feelincoding <feelincoding@gmail.com>
2022-05-18 14:42:43 +0900

    fix: mainheader, css

--- src/components/layout/header/MainHeader.vue
+++ src/components/layout/header/MainHeader.vue
@@ -20,10 +20,29 @@
           <li :class="{ on: navState.serviceState }" @click="changeNavState('serviceState')">
             <router-link :to="`${servicePath}`">서비스 관리</router-link>
           </li>
-          <li :class="{ on: navState.monitoringState }" @click="changeNavState('monitoringState')">
-            <router-link :to="`${mornitoringPath}`">모니터링</router-link>
+          <li :class="{ on: navState.monitoringState }" @click="changeShowMornitoring">
+            <a href="javascript:void(0)">모니터링</a>
+            <div class="depth-menu" v-if="navState.showMornitoring">
+              <ul>
+                <li>
+                  <i><img src="@/assets/user_ico.svg" alt="사용자 관리" /></i>
+                  <!-- <router-link :to="`${mornitoringPath}`">모니터링</router-link> -->
+                  <router-link :to="`${mornitoringPath}`" @click="changeNavState('monitoringState')">관제</router-link>
+                </li>
+                <li>
+                  <i><img src="@/assets/power_ico.svg" alt="권한관리" /></i>
+                  <a href="javascript:void(0)" @click="changeNavState('monitoringState')">통계</a>
+                </li>
+                <li>
+                  <i><img src="@/assets/power_ico.svg" alt="권한관리" /></i>
+                  <a href="javascript:void(0)" @click="changeNavState('monitoringState')">트래픽</a>
+                </li>
+              </ul>
+            </div>
           </li>
-          <li :class="{ on: navState.showManagement }" @click="navState.showManagement = !navState.showManagement">
+          <li :class="{ on: navState.managementState }" @click="changeShowManagement">
+            <!-- <button @click="navState.showManagement = !navState.showManagement">ss</button> -->
+            <!-- <a href="javascript:void(0)" @click="test2">Menagement</a> -->
             <a href="javascript:void(0)">Menagement</a>
             <div class="depth-menu" v-if="navState.showManagement">
               <ul>
@@ -66,10 +85,33 @@ interface NavState {
   apiState: boolean;
   serviceState: boolean;
   monitoringState: boolean;
+  managementState: boolean;
+  showMornitoring: boolean;
   showManagement: boolean;
 }
 @Component
 export default class MainHeader extends Vue {
+  changeShowMornitoring() {
+    if (!this.navState.showMornitoring) {
+      this.navState.showMornitoring = true;
+      if (this.navState.showManagement) {
+        this.navState.showManagement = false;
+      }
+    } else {
+      this.navState.showMornitoring = false;
+    }
+  }
+  changeShowManagement() {
+    if (!this.navState.showManagement) {
+      this.navState.showManagement = true;
+      if (this.navState.showMornitoring) {
+        this.navState.showMornitoring = false;
+      }
+    } else {
+      this.navState.showManagement = false;
+    }
+  }
+
   dashBoardPath = '/dashboard';
   systemPath = SYSTEM;
   apiPath = API;
@@ -83,6 +125,8 @@ export default class MainHeader extends Vue {
     apiState: false,
     serviceState: false,
     monitoringState: false,
+    managementState: false,
+    showMornitoring: false,
     showManagement: false,
   };
 
@@ -116,4 +160,76 @@ export default class MainHeader extends Vue {
   }
 }
 </script>
-<style></style>
+<style>
+#navi {
+  flex: 1;
+  margin-top: 12px;
+  text-align: right;
+  padding-right: 80px;
+}
+#navi ul li {
+  display: inline-block;
+  font-size: 18px;
+  font-weight: 500;
+  line-height: 30px;
+  margin-right: 70px;
+}
+#navi ul li:last-child {
+  margin-right: 0px;
+}
+#navi ul li a {
+  color: #333;
+}
+#navi ul li a:hover {
+  color: #8041f7;
+  text-decoration: none;
+}
+
+/* #navi ul li > .depth-menu {
+  display: none;
+} */
+
+/* #navi ul li.on {
+} */
+#navi ul li.on a {
+  color: #8041f7;
+}
+
+#navi ul li > .depth-menu {
+  display: block;
+  margin-top: 10px;
+  padding: 25px 25px;
+  width: 140px;
+  position: absolute;
+  background-color: #fff;
+  border-radius: 20px;
+  box-shadow: 1px 1px 5px #ccc;
+}
+#navi ul li > .depth-menu ul li {
+  display: block;
+  position: relative;
+  width: 100%;
+  height: 20px;
+  margin-bottom: 20px;
+  text-align: left;
+}
+#navi ul li > .depth-menu ul li i {
+  width: 20px;
+  height: 20px;
+  display: inline-block;
+}
+#navi ul li > .depth-menu ul li a {
+  display: inline-block;
+  font-size: 14px;
+  font-weight: 500;
+  line-height: 20px;
+  color: #333;
+}
+
+#navi ul li.on > .depth-menu ul li.active {
+}
+#navi ul li.on > .depth-menu ul li.active i {
+}
+#navi ul li.on > .depth-menu ul li.active a {
+}
+</style>
