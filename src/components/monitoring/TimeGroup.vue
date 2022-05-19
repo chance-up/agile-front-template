560b449284d938973eb30159b889790ddcbe574b
sloppy-cat <js3891129@gmail.com>
2022-05-19 15:58:50 +0900

    feat: 관제 시간선택 UI추가

---
+++ src/components/monitoring/TimeGroup.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="search-wrap monitor-search">
+    <div class="search-cont">
+      <h4 class="label-tit">기간 선택</h4>
+
+      <div class="radio-group">
+        <label class="radio-check" for="timeGroup24"
+          >24Hour
+          <input type="radio" name="timeGroup" value="radio" id="timeGroup24" />
+          <span class="radiomark"></span>
+        </label>
+        <label class="radio-check" for="timeGroup12"
+          >12Hour
+          <input type="radio" name="timeGroup" value="radio" id="timeGroup12" />
+          <span class="radiomark"></span>
+        </label>
+        <label class="radio-check" for="timeGroup6"
+          >6Hour
+          <input type="radio" name="timeGroup" value="radio" id="timeGroup6" />
+          <span class="radiomark"></span>
+        </label>
+        <label class="radio-check" for="timeGroup1"
+          >1Hour
+          <input type="radio" name="timeGroup" value="radio" id="timeGroup1" />
+          <span class="radiomark"></span>
+        </label>
+        <label class="radio-check" for="timeGroup30"
+          >30Min
+          <input type="radio" name="timeGroup" value="radio" id="timeGroup30" />
+          <span class="radiomark"></span>
+        </label>
+        <label class="radio-check" for="timeGroup10"
+          >10Min
+          <input type="radio" name="timeGroup" value="radio" id="timeGroup10" />
+          <span class="radiomark"></span>
+        </label>
+        <label class="radio-check" for="timeGroup5"
+          >5Min
+          <input type="radio" name="timeGroup" value="radio" id="timeGroup5" />
+          <span class="radiomark"></span>
+        </label>
+      </div>
+    </div>
+
+    <div class="time-wrap">
+      <span>2022.05.16 15:23:36</span>
+    </div>
+  </div>
+</template>
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component({})
+export default class ControlPage extends Vue {}
+</script>
--- src/pages/main/monitoring/children/controll-children/ControllAPIPage.vue
+++ src/pages/main/monitoring/children/controll-children/ControllAPIPage.vue
@@ -1,10 +1,11 @@
 <template>
-  <div>관제 API 탭</div>
+  <div><TimeGroup /></div>
 </template>
 <script>
 import { Component, Vue } from 'vue-property-decorator';
+import TimeGroup from '@/components/monitoring/TimeGroup.vue';
 
-@Component({})
+@Component({ components: { TimeGroup } })
 export default class ControlPage extends Vue {
   servicePath = '';
 }
