85934a9163822bac74a1ac8d0d4184b233c6d9c2
chance-up <ccs6473@naver.com>
2022-05-20 17:26:32 +0900

    Fix:시스템관리 버튼색 변경

--- src/pages/main/system-mngt/edit/SystemEditPage.vue
+++ src/pages/main/system-mngt/edit/SystemEditPage.vue
@@ -56,7 +56,7 @@
         </template>
         <template v-slot:modalFooter
           ><button class="lg-btn purple-btn" @click="onSubmit">{{ $t('common.ok') }}</button
-          ><button class="lg-btn purple-btn" @click="closeModal">{{ $t('common.cancel') }}</button>
+          ><button class="lg-btn white-btn" @click="closeModal">{{ $t('common.cancel') }}</button>
         </template>
       </ModalLayout>
     </template>
